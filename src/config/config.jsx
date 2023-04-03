69f0b6a394661650c17069a383e7fc5c76600cf2
Dicki Prasetya <dickiprasetya176@gmail.com>
2023-04-03 16:29:08 +0700

    feat: update backend

--- src/config/config.jsx
+++ src/config/config.jsx
@@ -1,6 +1,6 @@
 const config = {
   base_url: `https://pokeapi.co/api/v2`,
-  base_url_api: `http://localhost:8080/api`,
+  base_url_api: `evairo-crud-test.vercel.app/api`,
 };
 
 export default config;
--- src/redux/action/pokemonAction.jsx
+++ src/redux/action/pokemonAction.jsx
@@ -1,38 +1,47 @@
 import axios from "axios";
 import config from "../../config/config";
-import { setPokemon, setPokemonDetail } from "./globalAction";
+import { setLoading, setPokemon, setPokemonDetail } from "./globalAction";
 import { toast } from "react-toastify";
 
 const getAllPokemonAction = (offset, limit) => (dispatch) => {
+  dispatch(setLoading(true));
   axios
     .get(`${config.base_url}/pokemon?offset=${offset}&limit=${limit}`)
     .then((response) => {
       dispatch(setPokemon(response.data));
+      dispatch(setLoading(false));
     })
     .catch((error) => {
+      dispatch(setLoading(false));
       console.log(error);
     });
 };
 
 const getPokemonDetailAction = (id) => (dispatch) => {
+  dispatch(setLoading(true));
   axios
     .get(`${config.base_url}/pokemon/${id}`)
     .then((response) => {
       dispatch(setPokemonDetail(response.data));
+      dispatch(setLoading(false));
     })
     .catch((error) => {
+      dispatch(setLoading(false));
       console.log(error);
     });
 };
 
 const addTomyListPokemon = (value) => (dispatch) => {
+  dispatch(setLoading(true));
   axios
     .post(`${config.base_url_api}/pokemon/create`, value)
     .then((response) => {
+      dispatch(setLoading(false));
       toast.success("Create Succesfully");
       console.log(response);
     })
     .catch((error) => {
+      dispatch(setLoading(false));
       toast.success(error);
       console.log(error);
     });
